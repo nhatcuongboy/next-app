99853177c9324f5c8d5966c6d58a0f4b24b1e804
Christian <christian.nguyen@beyondedge.co>
2023-05-10 23:12:06 +0700

    Get static path

--- pages/post/[pid].tsx
+++ pages/post/[pid].tsx
@@ -1,10 +1,41 @@
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 
-const Post = () => {
+const Post = ({ post }: any) => {
     const router = useRouter()
     const { pid } = router.query
 
-    return <p>Post: {pid}</p>
+    const [data, setData] = useState('Loading');
+
+    setTimeout(() => {
+        setData(`My name is ${pid}`)
+    }, 1000);
+
+    return <>
+        <p>PID: {pid}</p>
+        <p>Name: {post?.name}</p>
+        <p>Data client: {data}</p>
+    </>
 }
 
 export default Post
+
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        // params is parsed from URL
+        paths: [{ params: { pid: '1' } }, { params: { pid: '2' } }, { params: { pid: '3' } }],
+        fallback: false,
+    };
+}
+
+export const getStaticProps: GetStaticProps = async (context: GetStaticPropsContext) => {
+    const pid = context.params?.pid
+
+    return {
+        props: {
+            post: { name: `I am ${pid}` }
+        },
+    };
+}
